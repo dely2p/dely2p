25819bbb31867441fa7c40e2b470d8bd60add42d
dely <lhyal2p@gmail.com>
2024-03-11 16:07:52 +0900

    update yml

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -12,8 +12,7 @@ let text = `# Hi there 👋
 ## 언어
 
 <p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
+  <img alt="" src= "https://img.shields.io/badge/Swift-DC6241?style=flat-square&logo=Swift&logoColor=white"/>
 </p>
 
 ## Contact me
